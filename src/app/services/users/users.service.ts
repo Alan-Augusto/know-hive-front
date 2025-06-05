efaf2d1327a251a3f52586115a4403571c91bbf6
alan-augusto <augustoalan56@gmail.com>
2025-06-05 15:03:36 -0300

    feat(users): implement CRUD methods for users service

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseApiService } from '../base-api.service';
+import { IUser } from '../../entity/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,25 @@ import { BaseApiService } from '../base-api.service';
 export class UsersService extends BaseApiService {
 
   override entityUrl = '/users';
+
+  create(data: IUser) {
+    return this.post('', data);
+  }
+
+  findAll() {
+    return this.get('');
+  }
+
+  findOne(id: string) {
+    return this.get(`/${id}`);
+  }
+
+  update(id: string, data: IUser) {
+    return this.patch(`/${id}`, data);
+  }
+
+  remove(id: string) {
+    return this.delete(`/${id}`);
+  }
+
 }
