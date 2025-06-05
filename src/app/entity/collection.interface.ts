3ea8719dbfdcb294a8aaa473a6aee84fe2884898
alan-augusto <augustoalan56@gmail.com>
2025-06-05 14:32:45 -0300

    feat(collections): add CRUD methods for collections service

---
+++ src/app/entity/collection.interface.ts
@@ -0,0 +1,8 @@
+export interface ICollection {
+  id?: string;
+  title: string;
+  description: string;
+  author_id: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
--- src/app/services/collections/collections.service.ts
+++ src/app/services/collections/collections.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseApiService } from '../base-api.service';
+import { ICollection } from '../../entity/collection.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,25 @@ import { BaseApiService } from '../base-api.service';
 export class CollectionsService extends BaseApiService {
 
   override entityUrl = '/collections';
+
+  create(data: ICollection) {
+    return this.post('', data);
+  }
+
+  findAll() {
+    return this.get('');
+  }
+
+  findOne(id: string) {
+    return this.get(`/${id}`);
+  }
+
+  update(id: string, data: ICollection) {
+    return this.patch(`/${id}`, data);
+  }
+
+  remove(id: string) {
+    return this.delete(`/${id}`);
+  }
+
 }
