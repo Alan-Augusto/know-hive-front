8a8027808a2369b4a9eb79ffc3eaee95219b00b6
alan-augusto <augustoalan56@gmail.com>
2025-06-05 14:59:48 -0300

    feat(questions): implement CRUD methods for questions service

---
+++ src/app/entity/question.interface.ts
@@ -0,0 +1,8 @@
+export interface IQuestion {
+  id?: string;
+  statement?: string;
+  type_id?: number;
+  created_at?: Date;
+  updated_at?: Date;
+  author_id?: string;
+}
--- src/app/services/questions/questions.service.ts
+++ src/app/services/questions/questions.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseApiService } from '../base-api.service';
+import { IQuestion } from '../../entity/question.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,25 @@ import { BaseApiService } from '../base-api.service';
 export class QuestionsService extends BaseApiService {
 
   override entityUrl = '/questions';
+
+  create(data: IQuestion) {
+    return this.post('', data);
+  }
+
+  findAll() {
+    return this.get('');
+  }
+
+  findOne(id: string) {
+    return this.get(`/${id}`);
+  }
+
+  update(id: string, data: IQuestion) {
+    return this.patch(`/${id}`, data);
+  }
+
+  remove(id: string) {
+    return this.delete(`/${id}`);
+  }
+
 }
