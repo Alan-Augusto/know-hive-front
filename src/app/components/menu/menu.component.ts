ae215a9576cb8e4ee76925b0a2a5197f5f44d0b9
alan-augusto <augustoalan56@gmail.com>
2025-06-06 15:48:42 -0300

    feat(menu): add click functionality to title for navigation to home

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonColorComponent } from "../button-color/button-color.component";
 import { ButtonTheme } from "../button-theme/button-theme.component";
 import { KhButtonComponent } from "../kh-button/kh-button.component";
 import { UserIconComponent } from "../user-icon/user-icon.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'menu',
@@ -12,7 +13,15 @@ import { UserIconComponent } from "../user-icon/user-icon.component";
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+  private router = inject(Router);
+
   isExpanded = signal(false);
   iconMenu = signal('ti ti-menu-2');
   titlePage = signal('🐝 KnowHive');
+
+  goHome(){
+    this.router.navigate(['/home']);
+  }
+
 }
