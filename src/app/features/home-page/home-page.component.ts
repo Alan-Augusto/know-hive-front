8936156e4f3e5273c81d8a1608784532c915f69e
alan-augusto <augustoalan56@gmail.com>
2025-06-09 16:05:31 -0300

    feat(home-v1): add home-v1 component with layout and statistics cards

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HomeV1Component } from "../home-v1/home-v1.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HomeV1Component],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
---
+++ src/app/features/home-v1/home-v1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-v1',
+  imports: [],
+  templateUrl: './home-v1.component.html',
+  styleUrl: './home-v1.component.scss'
+})
+export class HomeV1Component {
+
+}
