fb787662775c29e0f3f905e3596e9973c2a286e7
alan-augusto <augustoalan56@gmail.com>
2025-06-10 13:49:56 -0300

    fix(menu): remove unused KhButtonComponent import from menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,13 +2,12 @@ import { Component, computed, effect, inject, signal } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonColorComponent } from "../button-color/button-color.component";
 import { ButtonTheme } from "../button-theme/button-theme.component";
-import { KhButtonComponent } from "../kh-button/kh-button.component";
 import { UserIconComponent } from "../user-icon/user-icon.component";
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'menu',
-  imports: [TooltipModule, ButtonColorComponent, ButtonTheme, KhButtonComponent, UserIconComponent],
+  imports: [TooltipModule, ButtonColorComponent, ButtonTheme, UserIconComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
