5f2f9ade83f3d711cc8090b0ef2bd6c401e3dee1
alan-augusto <augustoalan56@gmail.com>
2025-06-09 21:40:40 -0300

    feat(menu): enhance menu item activation logic and update menu structure

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -20,21 +20,41 @@ export class MenuComponent {
   titlePage = signal('🐝 KnowHive');
 
   menuItems = signal([
-    // { label: 'Home', icon: 'ti ti-home', command: () => this.goHome() },
-    { id: 2, label: 'Início', url:'home', icon: 'ti ti-home', active:true},
-    { id: 2, label: 'Questões', url:'questions', icon: 'ti ti-help', active:false},
-    { id: 3, label: 'Coleções', url:'collections', icon: 'ti ti-folder', active:false},
-    { id: 4, label: 'Compartilhados Comigo', url:'shared-with-me', icon: 'ti ti-users', active:false},
-    { id: 5, label: 'Estatísticas', url:'statistics', icon: 'ti ti-chart-bar', active:false}
+    { id: 1, label: 'Início', url: 'home', icon: 'ti ti-home', active: false },
+    { id: 2, label: 'Questões', url: 'questions', icon: 'ti ti-help', active: false },
+    { id: 3, label: 'Coleções', url: 'collections', icon: 'ti ti-folder', active: false },
+    { id: 4, label: 'Compartilhados Comigo', url: 'shared-with-me', icon: 'ti ti-users', active: false },
+    { id: 5, label: 'Estatísticas', url: 'statistics', icon: 'ti ti-chart-bar', active: false }
   ]);
 
-  goHome(){
+  ngOnInit() {
+    this.setActiveMenuItemFromUrl();
+  }
+
+  setActiveMenuItemFromUrl() {
+    const currentUrl = this.router.url;
+    const activeItem = this.menuItems().find(item => currentUrl.includes(item.url));
+    if (activeItem) {
+      this.activateMenuItem(activeItem.id);
+    }
+  }
+
+  goHome() {
     this.router.navigate(['/home']);
   }
 
   redirectTo(url: string) {
     this.router.navigate([`/${url}`]);
     this.isExpanded.set(false);
+  }
+
+  activateMenuItem(itemId: number) {
+    this.menuItems.update(items => {
+      return items.map(item => ({
+        ...item,
+        active: item.id === itemId
+      }));
+    });
   }
 
 
