8c1a9abbf279614404c1d021669701767b044e58
alan-augusto <augustoalan56@gmail.com>
2025-06-25 20:42:40 -0300

    feat(home-v1): Refactor HomeTagCard component to include dynamic background and text colors,

--- src/app/features/home-v1/home-tag-card/home-tag-card.component.ts
+++ src/app/features/home-v1/home-tag-card/home-tag-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { IMostUsedTag } from '../../../entity/userStatistics.interface';
 
 @Component({
@@ -9,4 +9,28 @@ import { IMostUsedTag } from '../../../entity/userStatistics.interface';
 })
 export class HomeTagCardComponent {
   tag = input.required<IMostUsedTag>();
+  index = input.required<number>();
+
+  colorsList = signal<string[]>([
+    '#007bff',
+    '#a259ff',
+    '#00e18f',
+    '#ff6b6b',
+    '#ffc107',
+    '#28a745',
+    '#17a2b8',
+    '#fd7e14',
+    '#6f42c1'
+  ]);
+
+  backgroundColor = computed(() => {
+    // Adiciona opacidade de 10% (hex: 1A) ao final da cor
+    return this.colorsList()[this.index()] + '1A';
+  });
+
+  color = computed(() => {
+    return this.colorsList()[this.index()];
+  });
+
+  name = computed(() => {this.tag().name.replace(/-/g, ' ')});
 }
